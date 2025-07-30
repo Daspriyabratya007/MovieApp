272d6ed58e31ae67f49d293b61836a8f2ecc4bcf
Daspriyabratya007 <priyabratadas396@gmail.com>
2025-07-31 04:58:55 +0530

    Add Tailwind CSS setup and create Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <h1 className="text-2xl font-bold">Find <span className="bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text text-transparent">Movies</span> You Will Enjoy without the Hassle</h1>
+    </header>
+  );
+};
+
+export default Header;
